f18edf005fafa7207da6604081cd009096a079da
Eiki Martinson <eiki@eikimartinson.com>
2015-07-11 09:18:39 -0700

    This might be an improvement, now we have an enumerable list of colors instead

--- resistor.js
+++ resistor.js
@@ -4,25 +4,25 @@
  * July 10, 2015                                                                  *
  *--------------------------------------------------------------------------------*/
 
-function Color(name, value, isValidMultiplier, tolerance) {
-    this.name = name;
+function Color(value, isValidMultiplier, tolerance) {
     this.value = value;
     this.isValidMultiplier = isValidMultiplier;
     this.tolerance = tolerance;
 }
 
-var black  = new Color("black",   0, true,  null);
-var brown  = new Color("brown",   1, true,  "1");
-var red    = new Color("red",     2, true,  "2");
-var orange = new Color("orange",  3, true,  null);
-var yellow = new Color("yellow",  4, true,  null);
-var green  = new Color("green",   5, true,  "0.5");
-var blue   = new Color("blue",    6, true,  "0.25");
-var violet = new Color("violet",  7, true,  "0.10");
-var grey   = new Color("grey",    8, false, "0.05");
-var white  = new Color("white",   9, false, null);
-var gold   = new Color("gold",   -1, true,  "5");
-var silver = new Color("silver", -2, true, "10");
+var colors = {};
+colors["black"]  = new Color(0, true,  null);
+colors["brown"]  = new Color(1, true,  "1");
+colors["red"]    = new Color(2, true,  "2");
+colors["orange"] = new Color(3, true,  null);
+colors["yellow"] = new Color(4, true,  null);
+colors["green"]  = new Color(5, true,  "0.5");
+colors["blue"]   = new Color(6, true,  "0.25");
+colors["violet"] = new Color(7, true,  "0.10");
+colors["grey"]   = new Color(8, false, "0.05");
+colors["white"]  = new Color(9, false, null);
+colors["gold"]   = new Color(-1, true,  "5");
+colors["silver"] = new Color(-2, true, "10");
 
 function resistorValue(first, second, third, fourth, fifth) {
     
